ef7c5f270604c5eae044af1b8ceb96ad46339264
SilviaKCh <chivis.tt4@gmail.com>
2016-05-02 22:06:08 -0600

    formulario de prueba

---
+++ prueba_1_proyecto/prueba_1_proyecto/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace prueba_1_proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.txtHello = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Location = new System.Drawing.Point(46, 38);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnmostrar.TabIndex = 0;
+            this.btnmostrar.Text = "mostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.btnmostrar_Click);
+            // 
+            // txtHello
+            // 
+            this.txtHello.Location = new System.Drawing.Point(96, 106);
+            this.txtHello.Name = "txtHello";
+            this.txtHello.ReadOnly = true;
+            this.txtHello.Size = new System.Drawing.Size(176, 20);
+            this.txtHello.TabIndex = 1;
+            this.txtHello.Text = "\r\n";
+            this.txtHello.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtHello.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(34, 176);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 260);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtHello);
+            this.Controls.Add(this.btnmostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.TextBox txtHello;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ prueba_1_proyecto/prueba_1_proyecto/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba_1_proyecto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+            this.txtHello.Text += "Ingenieria del Software Prueba";
+
+        }
+    }
+}
