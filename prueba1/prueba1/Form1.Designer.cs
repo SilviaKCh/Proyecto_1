3e518126d4ddaaea494f5694f04c1be6774d5f7f
SilviaKCh <chivis.tt4@gmail.com>
2016-05-25 20:59:49 -0600

    changes

--- prueba1/prueba1/Form1.Designer.cs
+++ prueba1/prueba1/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(73, 117);
+            this.button1.Location = new System.Drawing.Point(48, 77);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -54,7 +54,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(178, 52);
+            this.button2.Location = new System.Drawing.Point(64, 126);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(106, 73);
             this.button2.TabIndex = 2;
